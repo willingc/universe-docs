dbd70d2f5150fe9291b6fcd39952bf3c186538ad
Carol Willing <carolcode@willingconsulting.com>
2016-02-01 16:40:14 -0800

    Add script and theme

--- source/conf.py
+++ source/conf.py
@@ -113,8 +113,24 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+import sphinx_bootstrap_theme
 
+html_theme = 'bootstrap'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {}
+html_theme_options = {
+    #'navbar_class': 'navbar-inverse',
+    #'navbar_sidebarrel': False,
+    'bootswatch_theme': 'cosmo',
+    'bootstrap_version': '3',
+}
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path =  sphinx_bootstrap_theme.get_html_theme_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -143,6 +159,13 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_sidebars = {
+    '**': [
+        'localtoc.html',
+    ]
+}
+
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
